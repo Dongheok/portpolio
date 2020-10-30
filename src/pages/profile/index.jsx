e6da41428e46ab3941169a64640c85903131ddde
dongheok <piouy_@naver.com>
2020-10-30 22:03:15 +0900

    ## 10/30 프로필 Ability 구현

--- src/pages/profile/index.jsx
+++ src/pages/profile/index.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect } from 'react';
 import { Grid } from '@material-ui/core';
-import { Wrapper, Skill } from './styles';
+import { Wrapper, SkillItem } from './styles';
 import Layout from './../../layout/';
 import Slider from 'react-slick';
 
@@ -20,30 +20,86 @@ const picture_data = [
    { key: 1, value: '/portpolio/images/profile/profile_picture_2.jpg' },
 ];
 
-const main_skill_data = [
-   { lang: 'HTML5' },
-   { lang: 'CSS3' },
-   { lang: 'SCSS' },
-   { lang: 'Javascript(ES6)' },
-   { lang: 'Jquery' },
-   { lang: 'Node-Js' },
-   { lang: 'Webpack' },
-   { lang: 'Babel' },
-   { lang: 'Next-Js' },
-   { lang: 'React(Hook)' },
-   { lang: 'Redux' },
-   { lang: 'Styled-components' },
-   { lang: 'Material-UI' },
-];
-const etc_skill_data = [
-   { lang: 'Express' },
-   { lang: 'Responsive-Web' },
-   { lang: 'Web-Standards' },
-   { lang: 'Cross-Browsing' },
-   { lang: 'Git' },
-   { lang: 'Jira' },
-   { lang: 'Photoshop' },
-   { lang: 'Illustrator' },
+const skill_data = [
+   { keyword: 'HM', lang: 'HTML5', color: 'green', explanation: 'HTML5등 DOCTYPE을 이해하고 문서에 맞춰 작업이 가능합니다.' },
+   { keyword: 'CS', lang: 'CSS3', color: 'blue', explanation: '어떤 디자인 시안이 와도 CSS3를 이용하여 100% 퀄리티로 구현할 수 있습니다.' },
+   {
+      keyword: 'SC',
+      lang: 'SCSS',
+      color: 'blue',
+      explanation: 'SCSS를 이용하여 효율적이며 코드의 재사용성을 높고 가독성이 높은 CSS를 작성할 수 있습니다.',
+   },
+   {
+      keyword: 'JS',
+      lang: 'Javascript',
+      color: 'green',
+      explanation: 'Javascript 언어의 기본문법과 사용법, 함수, 객체 등의 개념을 이해하여 동적인 프로그래밍을 할 수 있습니다.',
+   },
+   { keyword: 'JQ', lang: 'Jquery', color: 'red', explanation: '다양한 메소드를 사용해 기능을 구현할 수 있습니다..' },
+   { keyword: 'ES', lang: 'ES6', color: 'green', explanation: 'let, const, 스프레드 연산자, Arrow Function과 같은 ES6 기초를 이해합니다.' },
+   {
+      keyword: 'NJ',
+      lang: 'Node Js',
+      color: 'yellow',
+      explanation: 'Node JS의 기초를 이해하고, Node JS를 이용하여 프론트엔드 개발환경을 구축해 코로나맵 클론을 만들 수 있습니다.',
+   },
+   {
+      keyword: 'EX',
+      lang: 'Express',
+      color: 'red',
+      explanation: 'Express의 기초를 이해하고, Express를 이용하여 코로나맵 클론을 만들 수 있습니다.',
+   },
+   {
+      keyword: 'WP',
+      lang: 'Webpack',
+      color: 'yellow',
+      explanation: '웹팩의 동작 원리를 이해하고 직접 개발환경을 구성해봤습니다. 로더를 직접 구현해 보았습니다.',
+   },
+   {
+      keyword: 'BB',
+      lang: 'Babel',
+      color: 'yellow',
+      explanation: '바벨의 원리를 이해하고 직접 사용해 보아 프로젝트에 최적화된 환경을 구축해봤습니다.',
+   },
+   {
+      keyword: 'RT',
+      lang: 'React',
+      color: 'green',
+      explanation: 'React를 이용하여 정적페이지, CMS페이지, 플랫폼 등 여러 React 프로젝트 경험이 있습니다.',
+   },
+   { keyword: 'RD', lang: 'Redux', color: 'red', explanation: 'Redux를 이용하여 효율적으로 형상관리를 할 수 있습니다.' },
+   {
+      keyword: 'TS',
+      lang: 'Typescript',
+      color: 'yellow',
+      explanation: 'Typescript 기초를 이해하여 넷플릭스 클론 사이트를 만들 수 있습니다.',
+   },
+   { keyword: 'NJ', lang: 'Next Js', color: 'green', explanation: 'Next JS를 이용하여 프로젝트를 구현할 수 있습니다.' },
+   {
+      keyword: 'ST',
+      lang: 'Styled components',
+      color: 'blue',
+      explanation: 'Styled Component를 주로 사용하며, 보다 효율적으로 CSS를 작성할 수 있습니다.',
+   },
+   { keyword: 'MU', lang: 'Material UI', color: 'blue', explanation: 'Material UI라는 UI 프레임워크를 자주 씁니다.' },
+   { keyword: 'RW', lang: 'Responsive Web', color: 'red', explanation: '반응형 웹을 구현할 수 있습니다.' },
+   { keyword: 'RW', lang: 'Interactive Web', color: 'green', explanation: 'Javascript를 이용하여 애플 클론 페이지를 구현할 수 있습니다.' },
+   { keyword: 'WS', lang: 'Web Standards', color: 'yellow', explanation: '웹 표준을 지키며 코딩할 수 있습니다.' },
+   {
+      keyword: 'CB',
+      lang: 'Cross Browsing',
+      color: 'red',
+      explanation: '모든 브라우저에서 제가 맡은 프로젝트가 크로스 브라우징이 잘 되게 할 수 있습니다.',
+   },
+   { keyword: 'GT', lang: 'Git', color: 'yellow', explanation: 'Git을 이용하여 형상관리를 할 수 있습니다.' },
+   {
+      keyword: 'JR',
+      lang: 'Jira',
+      color: 'yellow',
+      explanation: '칸반과 스크럼을 이해하고 Jira를 이용하여 프로젝트 관리를 할 수 있습니다.',
+   },
+   { keyword: 'PS', lang: 'Photoshop', color: 'blue', explanation: '포토샵을 이용하여 프로젝트 시안을 만들 수 있습니다.' },
+   { keyword: 'IL', lang: 'Illustrator', color: 'red', explanation: '일러스트를 이용하여 간단한 아이콘, 로고 등을 만들 수 있습니다.' },
 ];
 
 const history_data = [
@@ -122,7 +178,7 @@ const Profile = () => {
                               간단소개
                            </Grid>
                            <Grid item className="text">
-                              안녕하세요! 산업기능요원으로 편입을 준비하는 신입 프론트엔드 개발자 김동혁입니다.
+                              안녕하세요! 산업기능요원 보충역(시력이슈)으로 편입을 준비하는 신입 프론트엔드 개발자 김동혁입니다.
                            </Grid>
                         </Grid>
                      </Grid>
@@ -132,13 +188,19 @@ const Profile = () => {
                   <Grid className="title">
                      <span>Ability</span>
                   </Grid>
-                  <Grid container justify="center" className="skill_wrap">
-                     <Grid item className="skill_item green">
-                        <Grid className="skill_content">
-                           <Grid className="title">HT</Grid>
-                           <Grid className="text">Html5</Grid>
-                        </Grid>
-                     </Grid>
+                  <Grid className="skill_item_wrap">
+                     {skill_data.map((x, index) => {
+                        return (
+                           <SkillItem key={index} className={x.color}>
+                              {/*  */}
+                              <Grid className="skill_item">
+                                 <Grid className="title">{x.keyword}</Grid>
+                                 <Grid className="text">{x.lang}</Grid>
+                              </Grid>
+                              <Grid className="skill_text">{x.explanation}</Grid>
+                           </SkillItem>
+                        );
+                     })}
                   </Grid>
                </Grid>
                <Grid className="education">
--- src/pages/profile/styles.jsx
+++ src/pages/profile/styles.jsx
@@ -1,11 +1,124 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+
+const FnText = css`
+   -webkit-background-clip: text;
+   -webkit-text-fill-color: transparent;
+`;
+
+export const SkillItem = styled.div`
+   display: inline-block;
+   position: relative;
+   width: 100px;
+   height: 100px;
+   padding: 4px;
+   margin: 0 5px 10px;
+   z-index: 1;
+   transition: all 0.3s ease;
+   & .skill_item {
+      background: #202e38;
+      padding: 10px 15px;
+      width: 100%;
+      height: 100%;
+      transition: inherit;
+
+      & > div {
+         text-align: left;
+         &.title {
+            font-size: 34px;
+            font-weight: 700;
+            margin: 0.3em 0 0;
+            transition: 0.8s ease 600ms;
+         }
+         &.text {
+            font-size: 12px;
+            margin-left: 2px;
+         }
+      }
+   }
+   & .skill_text {
+      position: absolute;
+      left: 50%;
+      top: 80%;
+      z-index: 10;
+      transform: translateX(-50%);
+      width: 180px;
+      height: auto;
+      line-height: 1.4;
+      padding: 10px;
+      text-align: center;
+      font-size: 12px;
+      color: #333;
+      border-radius: 6px;
+      border: 1px solid #eee;
+      background: #fff;
+      opacity: 0;
+      transition: opacity 0.3s ease 0.3s, top 0.3s cubic-bezier(0.175, 0.885, 0.32, 1.275) 0.3s;
+   }
+   /* 컬러 종류 */
+   &.red {
+      background: linear-gradient(to bottom right, #f953c6 0%, #b91d73 100%);
+      & > div {
+         & > div {
+            background: -webkit-linear-gradient(#f953c6, #b91d73);
+            ${FnText}
+         }
+      }
+   }
+   &.yellow {
+      background: linear-gradient(to bottom right, #f3f9a6 0%, #cbc634 100%);
+      & > div {
+         & > div {
+            background: -webkit-linear-gradient(#f3f9a6, #cbc634);
+            ${FnText}
+         }
+      }
+   }
+   &.blue {
+      background: linear-gradient(to bottom right, #00d2ff 0%, #3a7bd5 100%);
+      & > div {
+         & > div {
+            background: -webkit-linear-gradient(#00d2ff, #3a7bd5);
+            ${FnText}
+         }
+      }
+   }
+   &.green {
+      background: linear-gradient(to bottom right, #11998e 0%, #38ef7d 100%);
+      & > div {
+         & > div {
+            background: -webkit-linear-gradient(#11998e, #38ef7d);
+            ${FnText}
+         }
+      }
+   }
+
+   /* 호버 이벤트 */
+   &:hover {
+      transform: scale(1.12);
+      z-index: 10;
+      & .skill_item {
+         background: transparent;
+         & > div {
+            -webkit-text-fill-color: #222;
+            &.title {
+            }
+            &.text {
+            }
+         }
+      }
+      & .skill_text {
+         top: 105%;
+         opacity: 1 !important;
+      }
+   }
+`;
 
 export const Wrapper = styled.div`
    width: 100%;
    height: 100vh;
    background: #fff;
    background-size: cover;
-   padding: 80px 0 40px;
+   padding: 80px 20px 40px;
 
    & .profile {
       max-width: 1280px;
@@ -15,7 +128,7 @@ export const Wrapper = styled.div`
       & > div {
          width: 100%;
          padding: 32px;
-
+         margin-bottom: 48px;
          transition: all 0.4s;
          & > .title {
             text-align: center;
@@ -100,39 +213,21 @@ export const Wrapper = styled.div`
          }
       }
       & .skill {
-         padding-top: 60px;
-         & .skill_wrap {
-            padding-top: 24px;
-            & .skill_item {
-               width: 100px;
-               height: 100px;
-               padding: 4px;
-               margin: 0 4px 8px;
-               & .skill_content {
-                  height: 100%;
-                  padding: 10px 15px;
-                  background: #202e38;
-                  transition: all 0.3s ease;
-                  & .title {
-                     margin-top: 15px;
-                     font-size: 34px;
-                  }
-                  & .text {
-                     margin-top: 3px;
-                     font-size: 12px;
-                     letter-spacing: 0.5px;
-                  }
-               }
-               &.green {
-                  background: linear-gradient(to bottom right, #58ac30 0%, #a7df62 100%);
-                  & > div {
-                     & > div {
-                        background: -webkit-linear-gradient(#58ac30, #a7df62);
-                        font-weight: 700;
-                        -webkit-text-fill-color: transparent;
-                        -webkit-background-clip: text;
-                     }
-                  }
+         & .skill_item_wrap {
+            padding: 24px 0;
+            text-align: center;
+         }
+      }
+   }
+   @media (max-width: 600px) {
+      & .profile {
+         & .about {
+            & .info {
+               /* 사진 슬라이더 부분 */
+               & .picture_wrap {
+                  padding-right: 0;
+                  max-width: 200px;
+                  margin: 0 auto;
                }
             }
          }
