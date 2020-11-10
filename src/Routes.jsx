4068d97408eb7887a97d0f34fa0107e3f38a323e
dev-donghyuk <piouy_@naver.com>
2020-11-10 09:40:00 +0900

    ## url 경로이동 에러 수정

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,29 +1,35 @@
-import React, { useEffect } from 'react';
-import { BrowserRouter, Route, Switch, Redirect, useHistory } from 'react-router-dom';
-import Portpolio from './pages';
-import Project from './pages/project/';
-import Profile from './pages/profile/';
+import React, { useEffect } from "react";
+import {
+  BrowserRouter,
+  Route,
+  Switch,
+  Redirect,
+  useHistory,
+} from "react-router-dom";
+import Portpolio from "./pages";
+import Project from "./pages/project/";
+import Profile from "./pages/profile/";
 
 const Routes = () => {
-   const history = useHistory();
+  const history = useHistory();
 
-   useEffect(() => {
-      window.addEventListener('popstate', function (event) {
-         window.location.reload();
-         window.scrollTo(0, 0);
-      });
-   }, [history]);
-   return (
-      <BrowserRouter>
-         <Switch>
-            <Route path="/" exact component={Portpolio} />
-            <Route path="/project" exact component={Project} />
-            <Route path="/profile" exact component={Profile} />
+  useEffect(() => {
+    window.addEventListener("popstate", function (event) {
+      window.location.reload();
+      window.scrollTo(0, 0);
+    });
+  }, [history]);
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/portpolio" exact component={Portpolio} />
+        <Route path="/portpolio/project" exact component={Project} />
+        <Route path="/portpolio/profile" exact component={Profile} />
 
-            <Redirect path="*" to="/" exact />
-         </Switch>
-      </BrowserRouter>
-   );
+        <Redirect path="*" to="/portpolio" exact />
+      </Switch>
+    </BrowserRouter>
+  );
 };
 
 export default Routes;
--- src/common/index.jsx
+++ src/common/index.jsx
@@ -1,38 +1,41 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from "react";
 
 const { kakao } = window;
 
 export const MapApiComponent = (props) => {
-   useEffect(() => {
-      const container = document.getElementById('map');
-      const options = {
-         center: new kakao.maps.LatLng(37.6047395, 127.0946629),
-         level: 3,
-      };
+  const mapRef = useRef();
+  useEffect(() => {
+    const container = mapRef.current;
+    const options = {
+      center: new kakao.maps.LatLng(37.6047395, 127.0946629),
+      level: 3,
+    };
 
-      var markerPosition = new kakao.maps.LatLng(37.6047395, 127.0946629);
+    var markerPosition = new kakao.maps.LatLng(37.6047395, 127.0946629);
 
-      var marker = new kakao.maps.Marker({
-         position: markerPosition,
-      });
-      const map = new kakao.maps.Map(container, options);
+    var marker = new kakao.maps.Marker({
+      position: markerPosition,
+    });
+    const map = new kakao.maps.Map(container, options);
 
-      var infowindow = new kakao.maps.InfoWindow({
-         content: '<div style="width:150px;text-align:center;padding:6px 0;">집</div>',
-      });
-      infowindow.open(map, marker);
+    var infowindow = new kakao.maps.InfoWindow({
+      content:
+        '<div style="width:150px;text-align:center;padding:6px 0;">집</div>',
+    });
+    infowindow.open(map, marker);
 
-      marker.setMap(map);
-   }, []);
-   return (
-      <div className="popup">
-         <div
-            id="map"
-            style={{
-               width: '100%',
-               height: '500px',
-            }}
-         />
-      </div>
-   );
+    marker.setMap(map);
+  }, []);
+  return (
+    <div className="popup">
+      <div
+        id="map"
+        ref={mapRef}
+        style={{
+          width: "100%",
+          height: "500px",
+        }}
+      />
+    </div>
+  );
 };
