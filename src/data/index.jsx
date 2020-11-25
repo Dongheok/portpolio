e028098cf28d1bb599ee523e20327cf37c000ecb
dev-donghyuk <piouy_@naver.com>
2020-11-25 17:38:04 +0900

    ## 프로젝트 코로나맵 깃 주소 등록

--- src/data/index.jsx
+++ src/data/index.jsx
@@ -269,7 +269,7 @@ export const portPolioData = {
       img:
         "https://www.sphinfo.com/wp-content/uploads/2020/02/%E1%84%89%E1%85%B3%E1%84%8F%E1%85%B3%E1%84%85%E1%85%B5%E1%86%AB%E1%84%89%E1%85%A3%E1%86%BA-2020-02-04-%E1%84%8B%E1%85%A9%E1%84%92%E1%85%AE-5.04.31-1024x503.png",
       link: "",
-      git: "",
+      git: "https://github.com/dev-donghyuk/corona_map_clone",
       speed: 5,
       loading: true,
     },
