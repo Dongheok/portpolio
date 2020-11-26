6ec9300d37be13acf1c8b08bc3481aeb8af60edc
dev-donghyuk <piouy_@naver.com>
2020-11-26 17:23:38 +0900

    ## 로딩 svg 이미지 size 35% => 50% 변경

--- src/components/loading-bar/styles.jsx
+++ src/components/loading-bar/styles.jsx
@@ -1,4 +1,4 @@
-import styled, { keyframes } from 'styled-components';
+import styled, { keyframes } from "styled-components";
 
 const rotateAni = keyframes`
    100% {
@@ -19,36 +19,38 @@ const loadingAni = keyframes`
 `;
 
 const Wrapper = styled.div`
-   display: flex;
-   align-items: center;
-   justify-content: center;
-   position: fixed;
-   left: 0;
-   top: 0;
-   z-index: 1600;
-   width: 100%;
-   height: 100%;
-   background: rgba(255, 255, 255, 0.7) url('http://sn237x.cafe24.com/web/portpolio_img/dg_icon.png') no-repeat center center;
-   background-size: 35%;
-   /* background-size: 100vh; */
-   opacity: 0;
-   transition: all 0.5s;
-   &.on {
-      opacity: 1;
-   }
-   & .loading_circle {
-      width: 54px;
-      height: 54px;
-      transform: rotate(1deg);
-      animation: ${rotateAni} 3s infinite;
-      & circle {
-         stroke: black;
-         stroke-width: 4px;
-         stroke-dasharray: 157;
-         fill: none;
-         animation: ${loadingAni} 1s infinite;
-      }
-   }
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: fixed;
+  left: 0;
+  top: 0;
+  z-index: 1600;
+  width: 100%;
+  height: 100%;
+  background: rgba(255, 255, 255, 0.7)
+    url("http://sn237x.cafe24.com/web/portpolio_img/dg_icon.png") no-repeat
+    center center;
+  background-size: 50%;
+  /* background-size: 100vh; */
+  opacity: 0;
+  transition: all 0.5s;
+  &.on {
+    opacity: 1;
+  }
+  & .loading_circle {
+    width: 54px;
+    height: 54px;
+    transform: rotate(1deg);
+    animation: ${rotateAni} 3s infinite;
+    & circle {
+      stroke: black;
+      stroke-width: 4px;
+      stroke-dasharray: 157;
+      fill: none;
+      animation: ${loadingAni} 1s infinite;
+    }
+  }
 `;
 
 export default Wrapper;
