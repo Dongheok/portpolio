336fbc4bedd5ac8661f10e549eb9e0f7a9b29d81
dev-donghyuk <piouy_@naver.com>
2020-12-01 11:18:32 +0900

    ## 프로필 커리어, 스킬 텍스트 수정, 프로젝트 서브 프로젝트 텍스트 수정

--- src/data/profile/index.jsx
+++ src/data/profile/index.jsx
@@ -1,218 +1,241 @@
 export const profileData = {
-   // 프로필
-   //  - 사진 데이터
-   picture_data: [
-      {
-         key: 0,
-         value: 'http://sn237x.cafe24.com/web/portpolio_img/profile/profile_picture_1.jpg',
-      },
-      {
-         key: 1,
-         value: 'http://sn237x.cafe24.com/web/portpolio_img/profile/profile_picture_2.jpg',
-      },
-      {
-         key: 1,
-         value: 'http://sn237x.cafe24.com/web/portpolio_img/profile/profile_picture_3.jpg',
-      },
-   ],
-   //  - 스킬 데이터
-   skill_data: [
-      {
-         keyword: 'HM',
-         lang: 'HTML5',
-         color: 'main',
-         explanation: 'HTML5 시맨틱 태그, 메타 태그 등 기본문법을 이해하고 웹 표준에 맞춰 작업이 가능합니다.',
-      },
-      {
-         keyword: 'CS',
-         lang: 'CSS3',
-         color: 'sub',
-         explanation: '어떤 디자인 시안이 와도 CSS3를 이용하여 100% 퀄리티로 구현할 수 있습니다.',
-      },
-      {
-         keyword: 'SC',
-         lang: 'SCSS',
-         color: 'main',
-         explanation: 'SCSS를 이용하여 효율적이며 코드의 재사용성을 높고 가독성이 높은 CSS를 작성할 수 있습니다.',
-      },
-      {
-         keyword: 'JS',
-         lang: 'Javascript',
-         color: 'sub',
-         explanation: 'Javascript 언어의 기본문법과 사용법, 함수, 객체 등의 개념을 이해하여 동적인 프로그래밍을 할 수 있습니다.',
-      },
-      {
-         keyword: 'JQ',
-         lang: 'Jquery',
-         color: 'main',
-         explanation: 'Jquery의 다양한 메소드를 사용해 기능을 구현할 수 있습니다.',
-      },
-      {
-         keyword: 'ES',
-         lang: 'ES6',
-         color: 'sub',
-         explanation: 'let, const, 스프레드 연산자, Arrow Function과 같은 ES6 기초를 이해하고 ES6의 함수형 프로그래밍을 할 수 있습니다.',
-      },
-      {
-         keyword: 'NJ',
-         lang: 'Node Js',
-         color: 'main',
-         explanation: 'Node JS의 기초을 이해하고, Node JS를 이용하여 프론트엔드 개발환경을 구축해 코로나맵 클론을 만들 수 있습니다.',
-      },
-      {
-         keyword: 'EX',
-         lang: 'Express',
-         color: 'sub',
-         explanation: '(학습 예정)Express의 기초를 이해하고, Express를 이용하여 코로나맵 클론을 만들 수 있습니다.',
-      },
-      {
-         keyword: 'WP',
-         lang: 'webpack',
-         color: 'main',
-         explanation:
-            // !!!!!
-            '(학습 예정)웹팩의 동작 원리를 이해하고 직접 개발환경을 구성해봤습니다. 로더를 직접 구현해 보았습니다.',
-      },
-      {
-         keyword: 'BB',
-         lang: 'Babel',
-         color: 'sub',
-         explanation:
-            // !!!!!
-            '(학습 예정)바벨의 원리를 이해하고 직접 사용해 보아 프로젝트에 최적화된 환경을 구축해봤습니다.',
-      },
-      {
-         keyword: 'RT',
-         lang: 'React',
-         color: 'main',
-         explanation: 'React를 이용하여 정적페이지, CMS페이지, 플랫폼 등 여러 React 프로젝트 경험이 있습니다.',
-      },
-      {
-         keyword: 'RD',
-         lang: 'Redux',
-         color: 'sub',
-         explanation:
-            'Redux를 이용하여 효율적으로 형상관리를 할 수 있고, 엠플리파이어 환경 세팅없이 웹에 로그인 기능을 구현할 수 있습니다.',
-      },
-      {
-         keyword: 'TS',
-         lang: 'Typescript',
-         color: 'main',
-         explanation: 'Typescript 기초를 이해하여 영화 웹 사이트를 React와 Typescript를 사용하여 만들 수 있습니다.',
-      },
-      {
-         keyword: 'NJ',
-         lang: 'Next Js',
-         color: 'sub',
-         explanation: 'Next JS를 이용하여 SEO가 된 정적 페이지를 구현할 수 있습니다.',
-      },
-      {
-         keyword: 'ST',
-         lang: 'Styled components',
-         color: 'main',
-         explanation: 'React프로젝트를 진행할 때, Styled Component를 사용하여, 보다 효율적으로 CSS를 작성할 수 있습니다.',
-      },
-      {
-         keyword: 'MU',
-         lang: 'Material UI',
-         color: 'sub',
-         explanation: 'React프로젝트를 진행할 때, Material UI라는 UI 프레임워크를 사용합니다.',
-      },
-      {
-         keyword: 'RW',
-         lang: 'Responsive Web',
-         color: 'main',
-         explanation: '반응형 웹을 구현할 수 있습니다.',
-      },
-      {
-         keyword: 'IW',
-         lang: 'Interactive Web',
-         color: 'sub',
-         explanation: 'Javascript를 이용하여 애플 클론 페이지를 구현할 수 있습니다.',
-      },
-      {
-         keyword: 'WS',
-         lang: 'Web Standards',
-         color: 'main',
-         explanation: '웹 표준 이해하며, 웹 표준을 지키며 코딩할 수 있습니다.',
-      },
-      {
-         keyword: 'CB',
-         lang: 'Cross Browsing',
-         color: 'sub',
-         explanation: '진행하는 프로젝트가 모든 브라우저에서  크로스 브라우징이 잘 되게 할 수 있습니다.',
-      },
-      {
-         keyword: 'GT',
-         lang: 'Git',
-         color: 'main',
-         explanation: 'Git을 이용하여 형상관리를 할 수 있습니다.',
-      },
-      {
-         keyword: 'JR',
-         lang: 'Jira',
-         color: 'sub',
-         explanation: '칸반과 스크럼을 이해하고 Jira를 이용하여 프로젝트 관리를 할 수 있습니다.',
-      },
-      {
-         keyword: 'PS',
-         lang: 'Photoshop',
-         color: 'main',
-         explanation: '포토샵을 이용하여 프로젝트 시안을 만들 수 있습니다.',
-      },
-      {
-         keyword: 'IL',
-         lang: 'Illustrator',
-         color: 'sub',
-         explanation: '일러스트를 이용하여 간단한 아이콘, 로고 등을 만들 수 있습니다.',
-      },
-   ],
-   //  - 커리어 데이터
-   history_data: [
-      { years: '# 2018', history: ['송곡고등학교 졸업'] },
-      {
-         years: '# 2019',
-         history: [
-            '이젠컴퓨터학원(상봉) - 하이브리드 UI UI 웹 퍼블리셔 교육 이수',
-            'SV GROUP 입사',
-            'SV VIBA 정적페이지 리뉴얼 개발',
-            'SAMSUNG SSAFY UI 구현',
-            '파이어니 이벤트 페이지 디자인후 이벤트페이지 구현',
-         ],
-      },
-      {
-         years: '# 2020',
-         history: [
-            'SV DATA 반응형 정적페이지 개발',
-            'XY UI 정적페이지 개발',
-            '셔틀비 CMS 프론트엔드 개발',
-            '에그플레너 반응형 정적페이지 개발',
-            '플레이조커 프론트엔드 개발',
-            '손사찾기 반응형 정적페이지 프론트엔드 개발',
-            'SSAFYCMS 프론트엔드 개발',
-            '동네선수CMS 프론트엔드 개발',
-            '배달의딜러CMS 프론트엔드 개발',
-            '플레이조커CMS UI 구현',
-         ],
-      },
-   ],
-   //  - 자격증 데이터
-   certificate_data: [
-      {
-         title: '웹디자인 기능사',
-         date: '2019.09',
-         img: 'http://sn237x.cafe24.com/web/portpolio_img/profile/design_icon.png',
-      },
-      {
-         title: '정보처리 기능사',
-         date: '2020.07',
-         img: 'http://sn237x.cafe24.com/web/portpolio_img/profile/code_icon.png',
-      },
-   ],
-   //  - 문의하기 데이터
-   contact_data: [
-      { key: 'Phone', value: '010-3395-4623' },
-      { key: 'Email', value: 'piouy_@naver.com' },
-      { key: 'Git', value: 'https://github.com/dev-donghyuk' },
-   ],
+  // 프로필
+  //  - 사진 데이터
+  picture_data: [
+    {
+      key: 0,
+      value:
+        "http://sn237x.cafe24.com/web/portpolio_img/profile/profile_picture_1.jpg",
+    },
+    {
+      key: 1,
+      value:
+        "http://sn237x.cafe24.com/web/portpolio_img/profile/profile_picture_2.jpg",
+    },
+    {
+      key: 1,
+      value:
+        "http://sn237x.cafe24.com/web/portpolio_img/profile/profile_picture_3.jpg",
+    },
+  ],
+  //  - 스킬 데이터
+  skill_data: [
+    {
+      keyword: "HM",
+      lang: "HTML5",
+      color: "main",
+      explanation:
+        "HTML5 시맨틱 태그, 메타 태그 등 기본문법을 이해하고 웹 표준에 맞춰 작업이 가능합니다.",
+    },
+    {
+      keyword: "CS",
+      lang: "CSS",
+      color: "sub",
+      explanation:
+        "CSS의 기초를 이해하고, 어떤 디자인 시안이 와도 CSS3를 이용하여 100% 퀄리티로 구현할 수 있습니다.",
+    },
+    {
+      keyword: "SC",
+      lang: "SCSS",
+      color: "main",
+      explanation:
+        "SCSS의 변수, Nesting, Mixin등 기초를 이해하고, SCSS를 이용하여 코드의 재사용성을 높이고 가독성이 높은 CSS를 작성할 수 있습니다.",
+    },
+    {
+      keyword: "JS",
+      lang: "Javascript",
+      color: "sub",
+      explanation:
+        "Javascript 언어의 기본문법과 함수, 객체, 스코프, 실행환경, 클로저, 이벤트 등의 개념을 이해하여 동적인 프로그래밍을 할 수 있습니다.",
+    },
+    {
+      keyword: "E6",
+      lang: "ES6+",
+      color: "sub",
+      explanation:
+        "ES6의 let, const, 스프레드 연산자, Arrow Function 등 기초를 이해하고 map/filter/reduce, 파이프라인, 제너레이터를 통한 지연 평가, Promise 합성, 동시성/병렬성, async/await와 비동기 에러 핸들링를 이용해 이터러블/이터레이터/제너레이터 프로토콜을 응용하여 ES6의 함수형 프로그래밍을 할 수 있습니다.",
+    },
+    {
+      keyword: "JQ",
+      lang: "Jquery",
+      color: "main",
+      explanation:
+        "Jquery의 기초를 이해하여, CSS 스타일의 Selector을 이용한 DOM API를 이용하여 이벤트 처리나 애니메이션 효과를 유연하게 다룰 수 있다. Jquery를 이용하여 반응형 정적 페이지 배포한 경험이 있습니다.",
+    },
+    {
+      keyword: "NJ",
+      lang: "Node Js",
+      color: "main",
+      explanation:
+        "Node JS의 기초을 이해하고, Node JS를 이용하여 프론트엔드 개발환경을 구축할 수 있습니다.",
+    },
+    {
+      keyword: "EX",
+      lang: "Express",
+      color: "sub",
+      explanation:
+        "Express환경을 구축하고 코로나맵 클론을 구현한 경험이 있습니다.",
+    },
+    {
+      keyword: "WP",
+      lang: "Webpack",
+      color: "main",
+      explanation:
+        // !!!!!
+        "(학습 예정)Webpack의 동작 원리를 이해하고 기초를 이해하여, 로더를 직접 구현해 본 경험이 있습니다.",
+    },
+    {
+      keyword: "BB",
+      lang: "Babel",
+      color: "sub",
+      explanation:
+        // !!!!!
+        "(학습 예정)바벨의 원리를 이해하고 기초를 이해하여, 직접 바벨을 사용해 프로젝트에 최적화된 환경을 구축해 본 경험이 있습니다..",
+    },
+    {
+      keyword: "RC",
+      lang: "React",
+      color: "main",
+      explanation:
+        "React를 이용하여 정적페이지, CMS페이지, 플랫폼 등 여러 React 프로젝트를 구현한 경험이 있습니다.",
+    },
+    {
+      keyword: "RD",
+      lang: "Redux",
+      color: "sub",
+      explanation:
+        "Redux를 이용하여 효율적으로 형상관리를 할 수 있고, 엠플리파이어 환경 세팅없이 웹 애플리케이션에 로그인 기능을 구현한 경험이 있습니다.",
+    },
+    {
+      keyword: "TS",
+      lang: "Typescript",
+      color: "main",
+      explanation:
+        "Typescript 기초를 이해하여 React와 Typescript를 사용하여 반응형 영화 웹 애플리케이션 만들어 배포한 경험이 있습니다.",
+    },
+    {
+      keyword: "NJ",
+      lang: "Next Js",
+      color: "sub",
+      explanation:
+        "Next JS를 이용하여 SEO가 된 반응형 정적 페이지 구현하여 배포한 경험이 있습니다.",
+    },
+    {
+      keyword: "ST",
+      lang: "Styled components",
+      color: "main",
+      explanation:
+        "React 프로젝트를 진행할 때 주로 Styled Component를 사용해 보다 효율적으로 CSS를 작성합니다.",
+    },
+    {
+      keyword: "MU",
+      lang: "Material UI",
+      color: "sub",
+      explanation:
+        "React 프로젝트를 진행할 때 주로 Material UI 프레임워크 사용하여 효율적으로 컴포넌트를 구현합니다.",
+    },
+    {
+      keyword: "RW",
+      lang: "Responsive Web",
+      color: "main",
+      explanation:
+        "CSS를 직접 작성하여 반응형 웹 페이지를 구현한 경험이 있습니다.",
+    },
+    {
+      keyword: "IW",
+      lang: "Interactive Web",
+      color: "sub",
+      explanation:
+        "HTML5, CSS, Javascript를 이용하여 인터렉티브 웹 페이지를 구현한 경험이 있습니다.",
+    },
+    {
+      keyword: "WS",
+      lang: "Web Standards",
+      color: "main",
+      explanation:
+        "웹 표준 이해하여, 웹 표준을 지키며 프로젝트를 구현하여 W3C 웹표준 검사기에 통과한 경험이 있습니다.",
+    },
+    {
+      keyword: "CB",
+      lang: "Cross Browsing",
+      color: "sub",
+      explanation:
+        "진행하는 프로젝트가 웹 프로젝트를 모든 브라우저에서 크로스 브라우징이 잘 되게 구현한 경험이 있습니다.",
+    },
+    {
+      keyword: "GT",
+      lang: "Git",
+      color: "main",
+      explanation: "Git을 이용하여 형상관리를 한 경험이 있습니다.",
+    },
+    {
+      keyword: "JR",
+      lang: "Jira",
+      color: "sub",
+      explanation:
+        "칸반과 스크럼을 이해하고, 애자일 방법론 기반의 프로젝트 관리 도구 Jira를 설명하는 PPT를 만들어 발표한 경험이 있으며, Jira를 이용하여 프로젝트 관리를 할 수 있습니다.",
+    },
+    {
+      keyword: "PS",
+      lang: "Photoshop",
+      color: "main",
+      explanation:
+        "포토샵을 이용하여 홈페이지를 리뉴얼 웹 페이지 시안를 직접 디자인한 경험이 있습니다.",
+    },
+    {
+      keyword: "IL",
+      lang: "Illustrator",
+      color: "sub",
+      explanation:
+        "일러스트를 이용하여 간단한 아이콘, 로고 등을 만들어 사용한 경험이 있습니다.",
+    },
+  ],
+  //  - 커리어 데이터
+  history_data: [
+    { years: "# 2018", history: ["송곡고등학교 졸업"] },
+    {
+      years: "# 2019",
+      history: [
+        "이젠컴퓨터학원(상봉) - 하이브리드 UI UI 웹 퍼블리셔 교육 이수",
+        "SV GROUP 입사",
+        "SV VIBA Web 반응형 정적페이지 리뉴얼 개발",
+        "SSAFY Web UI 구현",
+        "PION Event Web 페이지 디자인 후 UI 구현",
+      ],
+    },
+    {
+      years: "# 2020",
+      history: [
+        "SV DATA Web 반응형 정적페이지 개발",
+        "XY Web 반응형 정적페이지 개발",
+        "ShuttleB CMS 프론트엔드 개발",
+        "에그플레너 Web 반응형 정적페이지 개발",
+        "PlayJoker Web 프론트엔드 개발",
+        "손사찾기 Web 반응형 정적페이지 프론트엔드 개발",
+        "SSAFY CMS 프론트엔드 개발",
+        "동네선수 CMS 프론트엔드 개발",
+        "배달의딜러 CMS 프론트엔드 개발",
+        "플레이조커 CMS UI 구현",
+      ],
+    },
+  ],
+  //  - 자격증 데이터
+  certificate_data: [
+    {
+      title: "웹디자인 기능사",
+      date: "2019.09",
+      img: "http://sn237x.cafe24.com/web/portpolio_img/profile/design_icon.png",
+    },
+    {
+      title: "정보처리 기능사",
+      date: "2020.07",
+      img: "http://sn237x.cafe24.com/web/portpolio_img/profile/code_icon.png",
+    },
+  ],
+  //  - 문의하기 데이터
+  contact_data: [
+    { key: "Phone", value: "010-3395-4623" },
+    { key: "Email", value: "piouy_@naver.com" },
+    { key: "Git", value: "https://github.com/dev-donghyuk" },
+  ],
 };
