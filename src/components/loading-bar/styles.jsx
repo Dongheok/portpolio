5e293566218f3a141a624fe91e6ba887cc3e5ed3
dongheok <piouy_@naver.com>
2020-12-11 18:38:52 +0900

    ## theme 수정

--- src/components/loading-bar/styles.jsx
+++ src/components/loading-bar/styles.jsx
@@ -1,4 +1,4 @@
-import styled, { keyframes } from "styled-components";
+import styled, { keyframes } from 'styled-components';
 
 const rotateAni = keyframes`
    100% {
@@ -19,38 +19,37 @@ const loadingAni = keyframes`
 `;
 
 const Wrapper = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  position: fixed;
-  left: 0;
-  top: 0;
-  z-index: 1600;
-  width: 100%;
-  height: 100%;
-  background: #fff url("http://sn237x.cafe24.com/web/portpolio_img/dg_icon.png")
-    no-repeat center center;
-  background-size: 50%;
-  visibility: hidden;
-  opacity: 0;
-  transition: all 0.4s;
-  &.on {
-    visibility: visible;
-    opacity: 1;
-  }
-  & .loading_circle {
-    width: 54px;
-    height: 54px;
-    transform: rotate(1deg);
-    animation: ${rotateAni} 3s infinite;
-    & circle {
-      stroke: black;
-      stroke-width: 4px;
-      stroke-dasharray: 157;
-      fill: none;
-      animation: ${loadingAni} 1s infinite;
-    }
-  }
+   display: flex;
+   align-items: center;
+   justify-content: center;
+   position: fixed;
+   left: 0;
+   top: 0;
+   z-index: 1600;
+   width: 100%;
+   height: 100%;
+   background: #fff url('http://sn237x.cafe24.com/web/portpolio_img/dg_icon.png') no-repeat center center;
+   background-size: 50%;
+   visibility: hidden;
+   opacity: 0;
+   transition: all 0.4s;
+   &.on {
+      visibility: visible;
+      opacity: 1;
+   }
+   & .loading_circle {
+      width: 54px;
+      height: 54px;
+      transform: rotate(1deg);
+      animation: ${rotateAni} 3s infinite;
+      & circle {
+         stroke: ${(props) => props.theme.black};
+         stroke-width: 4px;
+         stroke-dasharray: 157;
+         fill: none;
+         animation: ${loadingAni} 1s infinite;
+      }
+   }
 `;
 
 export default Wrapper;
