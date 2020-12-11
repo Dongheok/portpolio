9261cd2a4d8289ae2ea9d2f2b04e48f66f4998b5
dongheok <piouy_@naver.com>
2020-12-11 15:23:14 +0900

    ## 프로필 스킬 텍스트 수정, 헤더 노션 링크 추가

--- src/layout/styles.jsx
+++ src/layout/styles.jsx
@@ -1,76 +1,80 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 const Wrapper = styled.div`
-  /* 헤더 */
-  & .header {
-    overflow: hidden;
-    position: absolute;
-    left: 0;
-    top: 0;
-    width: 100%;
-    height: 80px;
-    line-height: 80px;
-    padding: 0 32px;
-    background: ${(props) => props.theme.bg};
-    & > div {
-      /* 로고 */
-      &.logo {
-        width: 62px;
-        height: 40px;
-        margin-top: 20px;
-        background: url("http://sn237x.cafe24.com/web/portpolio_img/logo.svg")
-          no-repeat;
-        text-indent: -99999px;
-        cursor: pointer;
-        &.on {
-          background: url("http://sn237x.cafe24.com/web/portpolio_img/logo_w.svg")
-            no-repeat;
-        }
-      }
-      /* 네비게이션 */
-      &.nav {
-        & > ul {
-          display: inline-block;
-          /* 글로벌 네비게이션 바 */
-          &.gnb {
-            & li {
-              display: inline-block;
-              margin-left: 24px;
-              & span {
-                display: block;
-                color: ${(props) => props.theme.main};
-                font-weight: 500;
-                font-size: 18px;
-                cursor: pointer;
-              }
+   /* 헤더 */
+   & .header {
+      overflow: hidden;
+      position: absolute;
+      left: 0;
+      top: 0;
+      width: 100%;
+      height: 80px;
+      padding: 0 32px;
+      background: ${(props) => props.theme.bg};
+      & > div {
+         /* 로고 */
+         &.logo {
+            width: 62px;
+            height: 40px;
+            margin-top: 20px;
+            background: url('http://sn237x.cafe24.com/web/portpolio_img/logo.svg') no-repeat;
+            text-indent: -99999px;
+            cursor: pointer;
+            &.on {
+               background: url('http://sn237x.cafe24.com/web/portpolio_img/logo_w.svg') no-repeat;
             }
-          }
-          /* 유틸 */
-          &.util {
-            padding-top: 11px;
-            & li {
-              display: inline-block;
-              padding: 0 0 0 24px;
-              & button {
-                position: relative;
-                & svg {
-                  color: ${(props) => props.theme.main};
-                  font-size: 32px;
-                }
-              }
-              & a {
-                display: block;
-                & svg {
-                  color: ${(props) => props.theme.main};
-                  font-size: 32px;
-                }
-              }
+         }
+         /* 네비게이션 */
+         &.nav_wrap {
+            & .nav {
+               & > div {
+                  /* 글로벌 네비게이션 바 */
+                  &.gnb_wrap {
+                     & .gnb {
+                        height: 80px;
+                        & div {
+                           margin-left: 24px;
+                           & span {
+                              color: ${(props) => props.theme.main};
+                              font-weight: 500;
+                              font-size: 18px;
+                              cursor: pointer;
+                           }
+                        }
+                     }
+                  }
+                  /* 유틸 */
+                  &.util_wrap {
+                     & .util {
+                        height: 80px;
+                        & div {
+                           padding: 0 0 0 24px;
+                           & button {
+                              height: 32px;
+                              & svg {
+                                 color: ${(props) => props.theme.main};
+                                 font-size: 32px;
+                              }
+                           }
+                           & a {
+                              display: block;
+                              & svg {
+                                 color: ${(props) => props.theme.main};
+                                 font-size: 32px;
+                              }
+                              & img {
+                                 width: 32px;
+                                 height: 32px;
+                              }
+                           }
+                        }
+                     }
+                  }
+               }
             }
-          }
-        }
+         }
       }
-    }
-  }
+   }
 `;
 
 export default Wrapper;
